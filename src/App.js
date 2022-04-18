9d6eecb514c1c830f90343be1a774d96a985c8e8
Cletus Kingdom <cletuskingdom@gmail.com>
2022-04-19 00:15:43 +0100

    added a links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component.jsx";
-
-const Navigation = () => {
-	return (
-		<div>
-			<div>
-				<h1>I am the Navigation bar</h1>
-			</div>
-			<Outlet />
-		</div>
-	);
-};
+import Navigation from "./routes/navigation/navigation.component.jsx";
 
 const Shop = () => {
 	return <h1>I am thhe shop page.</h1>;
