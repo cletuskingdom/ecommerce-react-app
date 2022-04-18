54477ba9ed39db52544dddb463cb34f200afddfa
Cletus Kingdom <cletuskingdom@gmail.com>
2022-04-19 00:05:39 +0100

    added a navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component.jsx";
 
+const Navigation = () => {
+	return (
+		<div>
+			<div>
+				<h1>I am the Navigation bar</h1>
+			</div>
+			<Outlet />
+		</div>
+	);
+};
+
+const Shop = () => {
+	return <h1>I am thhe shop page.</h1>;
+};
 const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />}></Route>
+			<Route path="/" element={<Navigation />}>
+				<Route index element={<Home />}></Route>
+				<Route path="/shop" element={<Shop />}></Route>
+			</Route>
 		</Routes>
 	);
 };
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
@@ -28,7 +29,12 @@ const Home = () => {
 			imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
 		},
 	];
-	return <Directory categories={categories} />;
+	return (
+		<div>
+			<Outlet />
+			<Directory categories={categories} />;
+		</div>
+	);
 };
 
 export default Home;
