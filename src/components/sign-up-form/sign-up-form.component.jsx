7ee86fd8b681ad3a7ed012fd762056623f30d837
Cletus Kingdom <cletuskingdom@gmail.com>
2022-04-19 13:41:56 +0100

    we can capture user's typed data

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,19 +1,62 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+	displayName: "",
+	email: "",
+	password: "",
+	confirmPassword: "",
+};
+
 const SignUpForm = () => {
+	const [formFields, setFormFields] = useState(defaultFormFields);
+	const { displayName, email, password, confirmPassword } = formFields;
+
+	console.log(formFields);
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setFormFields({ ...formFields, [name]: value });
+	};
+
 	return (
 		<div>
 			<h1>Sign up with your email and password</h1>
 			<form onSubmit={() => {}}>
 				<label>Display Name</label>
-				<input type="text" required />
+				<input
+					type="text"
+					required
+					onChange={handleChange}
+					value={displayName}
+					name="displayName"
+				/>
 
 				<label>Email</label>
-				<input type="email" required />
+				<input
+					type="email"
+					required
+					onChange={handleChange}
+					value={email}
+					name="email"
+				/>
 
 				<label>Password</label>
-				<input type="password" required />
+				<input
+					type="password"
+					required
+					onChange={handleChange}
+					value={password}
+					name="password"
+				/>
 
 				<label>Confirm password</label>
-				<input type="password" required />
+				<input
+					type="password"
+					required
+					onChange={handleChange}
+					value={confirmPassword}
+					name="confirmPassword"
+				/>
 
 				<button type="submit">Sign Up</button>
 			</form>
