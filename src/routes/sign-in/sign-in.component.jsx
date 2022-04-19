87229c873814d76ae3d7729bc909e042f3a93a08
Cletus Kingdom <cletuskingdom@gmail.com>
2022-04-19 01:33:08 +0100

    users can see a Sign-in with Google Pop up

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,13 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
 const SignIn = () => {
+	const logGoogleUser = async () => {
+		const response = await signInWithGooglePopup();
+		console.log(response);
+	};
 	return (
 		<div>
 			<h1>Sign in page</h1>
+			<button onClick={logGoogleUser}>Sign in with Google Popup</button>
 		</div>
 	);
 };
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+	getAuth,
+	signInWithRedirect,
+	signInWithPopup,
+	GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyA40FcrMbMjsUbxIeYtyYzatVfpImGQL1c",
+	authDomain: "crwn-clothing-db-b63ed.firebaseapp.com",
+	projectId: "crwn-clothing-db-b63ed",
+	storageBucket: "crwn-clothing-db-b63ed.appspot.com",
+	messagingSenderId: "923744861953",
+	appId: "1:923744861953:web:45a7591ff7a8b14b6ce500",
+};
+
+// Initialize Firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
